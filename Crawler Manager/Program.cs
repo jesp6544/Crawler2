56c0c44b216577f3d0280642082c57d9f7b5d482
Martin <martin94@hotmail.dk>
2016-11-14 10:18:16 +0100

    Added RunOnce registry code.

--- Crawler Manager/Program.cs
+++ Crawler Manager/Program.cs
@@ -1,35 +1,31 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Crawler_Manager
-{
-    static class Program
-    {
+namespace Crawler_Manager {
+
+    internal static class Program {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(string[] Args)
-        {
+        private static void Main(string[] Args) {
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunOnce", true);
+            rkApp.SetValue("Krawler Manager", Application.ExecutablePath + " 5");
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            if (Args.Length>=1)
-            try
-            {
-                if (int.Parse(Args.First()) > 0)
-                {
-                    Application.Run(new Form1(int.Parse(Args.First())));
-                    //MessageBox.Show("Run cmd line here");
-                }
-            }
-            catch (Exception) { }
-            
-            else
-            {
+            if(Args.Length >= 1)
+                try {
+                    if(int.Parse(Args.First()) > 0) {
+                        Application.Run(new Form1(int.Parse(Args.First())));
+                        //MessageBox.Show("Run cmd line here");
+                    }
+                } catch(Exception) { } else {
                 Application.Run(new Form1());
             }
         }
