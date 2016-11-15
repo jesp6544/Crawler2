6158149d1860c3b50a83e2ec9870139280577cce
Paul <git@wittig.dk>
2016-11-15 16:41:33 +0100

    Get & Put api works perfectly, AppVeyor still needs to be setup

--- CrawlerMVC/Migrations/Configuration.cs
+++ CrawlerMVC/Migrations/Configuration.cs
@@ -1,3 +1,6 @@
+using CrawlerLibrary.Models;
+using CrawlerMVC.Models;
+
 namespace CrawlerMVC.Migrations
 {
     using System;
@@ -16,8 +19,16 @@ namespace CrawlerMVC.Migrations
         protected override void Seed(CrawlerMVC.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
+            using (var ctx_mvc = new ApplicationDbContext())
+            {
+                if (ctx_mvc.SlaveControls.Any() == false) // Create initial dummy SlaveControl item for a fresh db
+                {
+                    ctx_mvc.SlaveControls.Add(new SlaveControl { TimeStamp = DateTime.Now });
+                    ctx_mvc.SaveChanges();
+                }
+            }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data. E.g.
             //
             //    context.People.AddOrUpdate(
--- CrawlerMVC/Models/IdentityModels.cs
+++ CrawlerMVC/Models/IdentityModels.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
+using System.Data.Entity.Core.Common;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
