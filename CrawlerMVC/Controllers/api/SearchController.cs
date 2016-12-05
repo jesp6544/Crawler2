93bfd4e088a0049130c34156b6c25fd5fb257a3e
Amivit <git@wittig.dk>
2016-12-05 10:46:12 +0100

    Moving HTMLContent class to library and starting the Search-api

---
+++ CrawlerMVC/Controllers/api/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using CrawlerLibrary.Models;
+using Microsoft.Practices.ServiceLocation;
+using SolrNet;
+using SolrNet.Commands.Parameters;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrawlerMVC.Controllers.api
+{
+    public class SearchController : ApiController
+    {
+        // slaves calling home: GET api/Slave
+        public IHttpActionResult Get()
+        {
+            SolrNet.Startup.InitContainer();
+            SolrNet.Startup.Init<HTMLContent>("http://176.23.159.28:8983/solr/testcore");
+
+            ISolrOperations<HTMLContent> solr = ServiceLocator.Current.GetInstance<ISolrOperations<HTMLContent>>();
+
+            var httpClient = new System.Net.Http.HttpClient(new HttpClientHandler());
+            //httpClient.
+
+            //var results =
+            return Ok(results);
+        }
+    }
+}
