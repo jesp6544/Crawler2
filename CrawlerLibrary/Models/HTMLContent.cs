ac86a086243d3a11896f41e09a8bc2e37b44f68a
Martin <martin94@hotmail.dk>
2016-12-06 13:17:20 +0100

    Broken shit. Please fix.

--- CrawlerLibrary/Models/HTMLContent.cs
+++ CrawlerLibrary/Models/HTMLContent.cs
@@ -1,11 +1,36 @@
 ﻿using SolrNet.Attributes;
+using System;
 using System.Collections.Generic;
 
-namespace CrawlerLibrary.Models
-{
-    public class HTMLContent
-    {
-        [SolrField("absoluteuri")]
+namespace CrawlerLibrary.Models {
+
+    public class HTMLContent {
+
+        [SolrField("id")]
+        public string ID { get; set; }
+
+        [SolrField("url")]
+        public string URL { get; set; }
+
+        [SolrField("title")]
+        public string Title { get; set; }
+
+        [SolrField("p")]
+        public ICollection<string> P { get; set; }
+
+        [SolrField("h1")]
+        public ICollection<string> H1 { get; set; }
+
+        [SolrField("h2")]
+        public ICollection<string> H2 { get; set; }
+
+        [SolrField("h3")]
+        public ICollection<string> H3 { get; set; }
+
+        [SolrField("publishdate")]
+        public string PublishDate { get; set; } = String.Format("{0:MM/dd/yyyy}", DateTime.Now);
+
+        /*[SolrField("absoluteuri")]
         public string AbsoluteUri { get; set; }
 
         [SolrUniqueKey("id")]
@@ -24,6 +49,6 @@ namespace CrawlerLibrary.Models
         public string PublishDate { get; set; }
 
         [SolrField("text")]
-        public ICollection<string> Text { get; set; }
+        public ICollection<string> Text { get; set; }*/
     }
 }
