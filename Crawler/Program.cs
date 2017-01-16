fa84338fce36195ff5b75772c0863c0c974297c7
Martin <martin94@hotmail.dk>
2017-01-16 20:17:25 +0100

    Added back global exception handling. Was for some reason gone?

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -8,30 +8,19 @@ namespace Crawler {
 
     internal static class Program {
         private static readonly DateTime StartTime = DateTime.Now;
-
-        public static Crawler Crawler
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
-        }
+        
 
         private static void Main() {
-            /*AppDomain.CurrentDomain.UnhandledException += delegate (object sender, UnhandledExceptionEventArgs args) {
+            AppDomain.CurrentDomain.UnhandledException += delegate (object sender, UnhandledExceptionEventArgs args) {
                 var e = (Exception)args.ExceptionObject;
                 Console.WriteLine(@"Unhandled exception: " + e);
                 ISolrOperations<HTMLContent> solr = ServiceLocator.Current.GetInstance<ISolrOperations<HTMLContent>>();
                 solr.Commit();
                 Environment.Exit(1);
-            };*/
+            };
 
             Startup.InitContainer();
-            Startup.Init<HTMLContent>("http://176.23.159.28:8983/solr/new_core2");
+            Startup.Init<HTMLContent>("http://192.168.1.130:8983/solr/new_core2");
             var crawler = new Crawler();
             var renderThread = new Thread(() => {
                 Render(crawler);
